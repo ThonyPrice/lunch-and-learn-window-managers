5dfa4ba7976892a5fa9d7d927ece274343298c34
Thony Price <thony@Thonys-MacBook-Pro.local>
2023-03-06 14:53:41 +0100

    Refine presentation

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env sh
 
-echo "Hello World!"
+echo "Installing yabai..."
+brew install koekeishiya/formulae/yabai
+
+echo "Installing shkd..."
+brew install koekeishiya/formulae/skhd
+
+echo "Adding Configs..."
+curl https://raw.githubusercontent.com/ThonyPrice/lunch-and-learn-window-managers/main/config/.yabairc > ~/.yabairc
+curl https://raw.githubusercontent.com/ThonyPrice/lunch-and-learn-window-managers/main/config/.shkdrc > ~/.shkdrc
---
+++ scripts/start.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+echo "Starting yabai and shkd..."
+brew services start yabai
+brew services start skhd
