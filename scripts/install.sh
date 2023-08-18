e0b273339de27e0e2541ee8080ce01a5cc384ed4
Thony Price <thony@Thonys-MacBook-Pro.local>
2023-08-18 11:29:56 +0200

    Update instructions for starting yabai and skhd
    Since both service migrated to use launchd instead of brew to run

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env sh
 
 echo "Installing yabai..."
-brew install koekeishiya/formulae/yabai
+brew install koekeishiya/formulae/yabai;
 
 echo "Installing shkd..."
-brew install koekeishiya/formulae/skhd
+brew install koekeishiya/formulae/skhd;
 
 echo "Adding Configs..."
-curl https://raw.githubusercontent.com/ThonyPrice/lunch-and-learn-window-managers/main/configs/.yabairc > ~/.yabairc
-curl https://raw.githubusercontent.com/ThonyPrice/lunch-and-learn-window-managers/main/configs/.skhdrc > ~/.skhdrc
+curl https://raw.githubusercontent.com/ThonyPrice/lunch-and-learn-window-managers/main/configs/.yabairc > ~/.yabairc;
+curl https://raw.githubusercontent.com/ThonyPrice/lunch-and-learn-window-managers/main/configs/.skhdrc > ~/.skhdrc;
